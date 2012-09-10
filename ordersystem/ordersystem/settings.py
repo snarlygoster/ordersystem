ea4e29ff0df4036d55a514fd1e7f0deaef4daa86
David Eves <deves@pobox.com>
2012-09-10 15:05:23 -0700

    create order application

--- ordersystem/ordersystem/settings.py
+++ ordersystem/ordersystem/settings.py
@@ -126,6 +126,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'fts',
+    'orders',
 )
 
 # A sample logging configuration. The only tangible logging
